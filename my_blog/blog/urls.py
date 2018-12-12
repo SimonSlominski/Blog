f2e8d87194d6496fa4d4840026a2e619023be186
Szymon Słomiński <simon.slominski@gmail.com>
2018-12-12 11:19:28 +0100

    add PostDetailView

---
+++ my_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from .views import PostListView, PostDetailView
+
+urlpatterns = [
+    url(r'^$', PostListView.as_view(), name='post_list'),
+    url(r'^(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='post_detail'), # URL visible as slug
+    # url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'), # URL visible as pk/id
+    
+
+]
+
