1d497fc207017ddd0d5e9a2073b9c8e31b1a1f15
Szymon Słomiński <simon.slominski@gmail.com>
2018-12-21 00:15:53 +0100

    Add TagListView and add URL for this view

---
+++ my_blog/blog/mixins.py
@@ -0,0 +1,9 @@
+from taggit.models import Tag
+
+class TagMixin(object):
+
+    def get_context_data(self, **kwargs):
+        context = super(TagMixin, self).get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, post_share
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, \
+    post_share, TagListView
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='post_list'),
+    url(r'^tag/(?P<slug>[-\w]+)/$', TagListView.as_view(), name='tagged'),
     url(r'^(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='post_detail'), # URL visible as slug
     # url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'), # URL visible as pk/id
     url(r'^blog/add/$', PostCreateView.as_view(), name='post_create'),
