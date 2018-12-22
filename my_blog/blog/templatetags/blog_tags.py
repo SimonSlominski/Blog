7015ecedc1d058cc043050a0528fd22e0e28cc81
Szymon Słomiński <simon.slominski@gmail.com>
2018-12-22 13:08:49 +0100

    Update templatetags

--- my_blog/blog/templatetags/blog_tags.py
+++ my_blog/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
-from django import template
+from django.utils.safestring import mark_safe
 from django.db.models import Count
+from django import template
+import markdown
 
 from ..models import Post
 
@@ -23,4 +25,9 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
     """Show most commented posts."""
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
